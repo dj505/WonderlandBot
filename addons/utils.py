d73e1fce54c84ae60268cad0ae8a660618e57c49
dj505 <dj505@users.noreply.github.com>
2018-01-28 22:53:57 -0700

    Added source code link command

--- addons/utils.py
+++ addons/utils.py
@@ -89,5 +89,12 @@ class Utils:
         embed.set_thumbnail(url=ctx.message.server.icon_url)
         await self.bot.say(embed=embed)
 
+    @commands.command(pass_context=True, brief='A Link to the Source')
+    async def source(self, ctx):
+        embed = discord.Embed(name='Bot Source Code', description='Have some spaghetti code!', color=0x00FF99)
+        embed.set_thumbnail(url='https://opensource.org/files/osi_keyhole_600X600_90ppi.png')
+        embed.add_field(name='GitHub Repository', value='https://github.com/dj505/WonderlandBot', inline=True)
+        await seld.bot.say(embed=embed)
+
 def setup(bot):
     bot.add_cog(Utils(bot))
